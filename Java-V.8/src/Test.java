cc405b342d1bffc4984a0496e66757b2877a8cc5
RoshiniAjoy797 <Roshini@DESKTOP-2869A08>
2024-08-19 16:22:20 +0530

    Run-time polymorphism

---
+++ Java-V.8/src/Test.java
@@ -0,0 +1,10 @@
+
+public class Test {
+public static void main(String[] args) {
+		Animal an = new Cat();
+		System.out.println(an.makeASound());
+		Animal anm = new Dog();
+		anm.makeASound();
+	}
+
+}
